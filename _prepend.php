72a702485fb8e7e69d24571aa06e33ea67e9901f
Jean-Christian Denis <contact@jcdenis.fr>
2021-10-23 23:37:44 +0200

    remove old plugin compatibility

--- _prepend.php
+++ _prepend.php
@@ -15,27 +15,9 @@ if (!defined('DC_RC_PATH')) {
     return null;
 }
 
-# Check Dotclear version
-if (!method_exists('dcUtils', 'versionsCompare') 
- || dcUtils::versionsCompare(DC_VERSION, '2.18', '<', false)) {
-    return null;
-}
-
 $d = dirname(__FILE__) . '/inc/';
 
 # DB class
-$__autoload['periodical'] = $d . 'class.periodical.php';
+$__autoload['periodical']          = $d . 'class.periodical.php';
 # Admin list and pagers
 $__autoload['adminPeriodicalList'] = $d . 'lib.index.pager.php';
-
-# Add to plugn soCialMe (writer part)
-$__autoload['periodicalSoCialMeWriter'] = $d . 'lib.periodical.socialmewriter.php';
-
-$core->addBehavior(
-    'soCialMeWriterMarker',
-    ['periodicalSoCialMeWriter', 'soCialMeWriterMarker']
-);
-$core->addBehavior(
-    'periodicalAfterPublishedPeriodicalEntry',
-    ['periodicalSoCialMeWriter', 'periodicalAfterPublishedPeriodicalEntry']
-);
